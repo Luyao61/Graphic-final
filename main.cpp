9e2b775eff20258dad6e12c79c9a2343778778c0
Luyao Zhou <luyao912@gmail.com>
2015-11-15 22:15:46 -0800

    rename project

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     glutInit(&argc, argv);                                      //Initialize GLUT
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);   //Open an OpenGL context with double buffering, RGB colors, and depth buffering
     glutInitWindowSize(Window::width, Window::height);          //Set initial window size
-    glutCreateWindow("Texture-Mapping"); //Open window and set window title
+    glutCreateWindow("CSE 167 Final Project"); //Open window and set window title
     
     glEnable(GL_DEPTH_TEST);                                    //Enable depth buffering
     glClear(GL_DEPTH_BUFFER_BIT);                               //Clear depth buffer
